b3742e0512cb0bc50e4320f2d2da16ecaf4fecda
Arvid Gärdebo <gardeboarvid@gmail.com>
2023-02-13 15:44:09 +0100

    Arvid

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const btn = document.querySelector("button");
+const h2 = document.querySelector("h2");
+const gameOverScreen = document.getElementById("game-over");
+
 const game = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 const square1 = document.getElementById("1");
@@ -45,98 +49,68 @@ squares.forEach((el, index) => {
   });
 });
 
+const playerWins = (player) => {
+  gameOverScreen.classList.remove("hidden");
+
+  if (player === "draw") {
+    h2.textContent = `It's a draw!`;
+  } else {
+    h2.textContent = `Player ${player} wins!`;
+  }
+};
+
+btn.addEventListener("click", (e) => {});
+
 function checkIfWin(player) {
   if (
     (game[0] == 1 && game[1] == 1 && game[2] == 1) ||
     (game[0] == 2 && game[1] == 2 && game[2] == 2)
   ) {
-    console.log(`Player ${player} wins!`);
+    playerWins(player);
   }
   if (
     (game[3] == 1 && game[4] == 1 && game[5] == 1) ||
     (game[3] == 2 && game[4] == 2 && game[5] == 2)
   ) {
-    console.log(`Player ${player} wins!`);
+    playerWins(player);
   }
   if (
     (game[6] == 1 && game[7] == 1 && game[8] == 1) ||
     (game[6] == 2 && game[7] == 2 && game[8] == 2)
   ) {
-    console.log(`Player ${player} wins!`);
+    playerWins(player);
   }
   if (
     (game[0] == 1 && game[3] == 1 && game[6] == 1) ||
     (game[0] == 2 && game[3] == 2 && game[6] == 2)
   ) {
-    console.log(`Player ${player} wins!`);
+    playerWins(player);
   }
   if (
     (game[1] == 1 && game[4] == 1 && game[7] == 1) ||
     (game[1] == 2 && game[4] == 2 && game[7] == 2)
   ) {
-    console.log(`Player ${player} wins!`);
+    playerWins(player);
   }
   if (
     (game[2] == 1 && game[5] == 1 && game[8] == 1) ||
     (game[2] == 2 && game[5] == 2 && game[8] == 2)
   ) {
-    console.log(`Player ${player} wins!`);
+    playerWins(player);
   }
   if (
     (game[0] == 1 && game[4] == 1 && game[8] == 1) ||
     (game[0] == 2 && game[4] == 2 && game[8] == 2)
   ) {
-    console.log(`Player ${player} wins!`);
+    playerWins(player);
   }
   if (
     (game[2] == 1 && game[4] == 1 && game[6] == 1) ||
     (game[2] == 2 && game[4] == 2 && game[6] == 2)
   ) {
-    console.log(`Player ${player} wins!`);
+    playerWins(player);
   }
   if (game.every((el) => el >= 1)) {
-    console.log(`It's a draw!`);
+    playerWins("draw");
   }
 }
-const winConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-// function checkIfWin(player)
-// [player, player, player]
-// [0, 0, 0]
-// [0, 0, 0]
-
-// [0, 0, 0]
-// [player, player, player]
-// [0, 0, 0]
-
-// [0, 0, 0]
-// [0, 0, 0]
-// [player, player, player]
-
-// [player, 0, 0]
-// [player, 0, 0]
-// [player, 0, 0]
-
-// [player, 0, 0]
-// [0, player, 0]
-// [0, 0, player]
-
-// [0, player, 0]
-// [0, player, 0]
-// [0, player, 0]
-
-// [0, 0, player]
-// [0, player, 0]
-// [player, 0, 0]
-
-// [0, 0, player]
-// [0, 0, player]
-// [0, 0, player]
