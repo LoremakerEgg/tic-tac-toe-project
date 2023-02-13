760de73613a55ca0bd6f7c13b243d431e9b837f4
Arvid Gärdebo <gardeboarvid@gmail.com>
2023-02-13 13:52:38 +0100

    arvid

---
+++ script.js
@@ -0,0 +1,38 @@
+const game = [
+  [0, 0, 1],
+  [0, 2, 0],
+  [0, 0, 0],
+];
+
+// function checkIfWin(player)
+// [player, player, player]
+// [0, 0, 0]
+// [0, 0, 0]
+
+// [0, 0, 0]
+// [player, player, player]
+// [0, 0, 0]
+
+// [0, 0, 0]
+// [0, 0, 0]
+// [player, player, player]
+
+// [player, 0, 0]
+// [player, 0, 0]
+// [player, 0, 0]
+
+// [player, 0, 0]
+// [0, player, 0]
+// [0, 0, player]
+
+// [0, player, 0]
+// [0, player, 0]
+// [0, player, 0]
+
+// [0, 0, player]
+// [0, player, 0]
+// [player, 0, 0]
+
+// [0, 0, player]
+// [0, 0, player]
+// [0, 0, player]
